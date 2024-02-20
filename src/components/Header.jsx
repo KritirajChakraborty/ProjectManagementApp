76c73a1654267c02e7a0a7bc9d551995b4ba21d2
KritirajChakraborty <156446954+KritirajChakraborty@users.noreply.github.com>
2024-02-20 18:45:08 +0530

    Delete src directory

--- src/components/Header.jsx
+++
@@ -1,7 +0,0 @@
-export default function Header(){
-    return (
-        <header>
-            <h1 className="mx-8 my-12 text-center text-2xl font-bold md:text-4xl text-gray-900">PROJECT MANAGEMENT APPLICATION</h1>
-        </header>
-    )
-}
